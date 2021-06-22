3bc8ea35fc1740353bd2d8e7e82ce1b8efc4fd82
petrepan <peterawotola@gmail.com>
2021-06-22 15:22:38 +0100

    refactor-authentication

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -8,13 +8,6 @@ import {
 } from "../conversations";
 import { gotUser, setFetchingStatus } from "../user";
 
-axios.interceptors.request.use(async function (config) {
-  const token = await localStorage.getItem("messenger-token");
-  config.headers["x-access-token"] = token;
-
-  return config;
-});
-
 // USER THUNK CREATORS
 
 export const fetchUser = () => async (dispatch) => {
